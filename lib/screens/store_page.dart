302cb793bbb43cb318d49b4dfdace05a31c7fc1a
oreste <muhirwa.g.oreste@gmail.com>
2022-10-07 12:29:32 +0200

    This page will only contain movies, tv,.. that are not included in prime, thet have to be rent, or bought

---
+++ lib/screens/store_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class StorePage extends StatefulWidget {
+  const StorePage({Key? key}) : super(key: key);
+
+  @override
+  State<StorePage> createState() => _StorePageState();
+}
+
+class _StorePageState extends State<StorePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
